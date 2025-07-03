14b4ecd4b7597d1d1efa334cf85caeb9152d4119
1F10240197 <s1f102401977@iniad.org>
2025-07-03 11:45:01 +0900

    詳細ページを追加

--- todo/views.py
+++ todo/views.py
@@ -22,7 +22,7 @@ def index(request):
 
 def detail(request, task_id):
     try:
-        task = Tak.objects.get(pk=task_id)
+        task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
 
